8eb8bcfcdb67e82ff79dc55140050d1ec6008142
Akshay S Mathad <akshaymathad4@gmail.com>
2019-02-25 13:49:08 +0530

    prod

---
+++ assets/theme/js/nav-footer.js
@@ -0,0 +1,190 @@
+
+var header = '<section class="menu1" id="menu-0">'+
+'			<nav class="navbar navbar-dropdown navbar-fixed-top">'+
+'				<div class="container-fluid">'+
+''+
+'					<div class="mbr-table">'+
+'						<div class="mbr-table-cell">'+
+''+
+'							<div class="navbar-brand">'+
+'								<a href="index.html" class="navbar-logo"><img src="assets/images/cf-logo-transparent.png" alt="Mobirise"></a>'+
+''+
+'							</div>'+
+''+
+'						</div>'+
+'						<div class="mbr-table-cell">'+
+''+
+'							<button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">'+
+'								<span class="hamburger-icon"></span>'+
+'							</button>'+
+''+
+'							<ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="services.html"> Services </a>'+
+'								</li>'+
+'								<li class="nav-item dropdown">'+
+'									<a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="https://mobirise.com/" aria-expanded="false">Incorporation</a>'+
+'									<div class="dropdown-menu">'+
+'										<a class="dropdown-item" href="start-business.html">Start A Business</a>'+
+'										<a class="dropdown-item" href="foreign-registration.html">Foreign Company Registration</a>'+
+'									</div>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="registration.html"> Registration </a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="manage-business.html"> Manage Business </a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="gst.html"> GST </a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="company-law.html"> Company Law </a>'+
+'								</li>'+
+'								<li class="nav-item dropdown">'+
+'									<a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" aria-expanded="false">Startup Corner</a>'+
+'									<div class="dropdown-menu">'+
+'										<a class="dropdown-item" href="cfo-on-call.html">CFO on Call</a>'+
+'										<a class="dropdown-item" href="startup-package.html">Startup Package</a>'+
+'										<a class="dropdown-item" href="legal-documents.html">Legal Documents</a>'+
+'									</div>'+
+'								</li>'+
+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" href="funding.html" aria-expanded="false">Finance</a>'+
+'								</li>'+
+'								<li class="nav-item">'+
+'									<a class="nav-link link" aria-expanded="false" href="knowledge-bank.html"> Knowledge Bank </a>'+
+'								</li>'+
+'							</ul>'+
+'							<button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">'+
+'								<span class="close-icon"></span>'+
+'							</button>'+
+''+
+'						</div>'+
+'					</div>'+
+''+
+'				</div>'+
+'			</nav>'+
+''+
+'		</section>';
+	
+
+var footer = '<footer class="mbr-small-footer mbr-section mbr-section-nopadding footer1" id="footer3-b" style="background-color: #2C3338; padding-top: 1.75rem; padding-bottom: 1.75rem;">'+
+
+'			<div align="right">'+
+'				<p style="font-size: 15px; font-family: \'Montserrat\'; color: #fff" >'+
+'					INTERNATIONAL NETWORK PARTNERS PRESENCE AT : &nbsp;&nbsp;<img src="assets/images/singapore.png" style="height: 15px;">&nbsp;&nbsp; SINGAPORE, &nbsp;&nbsp;<img src="assets/images/mauritius.png" style="height: 15px;">&nbsp;&nbsp;  MAURITIUS, &nbsp;&nbsp;<img src="assets/images/maldives.png" style="height: 15px;">&nbsp;&nbsp;  MALDIVES & &nbsp;&nbsp;<img src="assets/images/uae.png" style="height: 15px;">&nbsp;&nbsp;  DUBAI &nbsp;'+
+'				</p>'+
+'			</div>'+
+
+'			<div class="container-fluid">'+
+'<div class="row">'+
+'					<div class="col-xs-12 col-lg-12 ">'+
+'						<div class="footer-list col-xs-12 col-lg-12">'+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<div class="logo" style="margin-top: 40px;">'+
+'									<a href="index.html"><img src="assets/images/cf-logo-transparent.png" alt="Mobirise" style="width: 95%;"></a>'+
+'								</div>'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Follow Us'+
+'								</p>'+
+'								<div class="mbr-section-btn">'+
+'									<a class="btn btn-icon" href="https://www.facebook.com/VidyasSunilAssociates" target="_blank"> <span class="socicon socicon-facebook mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://twitter.com/VidyaSunil" target="_blank"> <span class="socicon socicon-twitter mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://www.linkedin.com/in/vidyasunil" target="_blank"> <span class="socicon socicon-linkedin mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="http://vidyasunil.blogspot.in" target="_blank"> <span class="socicon socicon-blogger mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://plus.google.com/u/0/+VidyaSunil" target="_blank"> <span class="socicon socicon-google mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://www.instagram.com/vidyasunilassociates/" target="_blank"> <span class="socicon socicon-instagram mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'									<a class="btn btn-icon" href="https://vidyasunilassociates.wordpress.com" target="_blank"> <span class="socicon socicon-wordpress mbr-iconfont mbr-iconfont-btn"></span></a>'+
+'								</div>'+
+
+'							</div>'+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Quick Links'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://www.mca.gov.in">Ministry of Corporate Affairs</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://www.gst.gov.in">GST</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://incometaxindiaefiling.gov.in">Income Tax</a>'+
+'								</p>'+
+''+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://startupindia.gov.in">Startup India</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://dipp.nic.in">Department of Industrial Policy & Promotion</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://msme.gov.in">Ministry of Small & Medium Enterprise</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://niti.gov.in">Niti Aayog</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://www.sidbi.in">SIDBI </a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://www.mudra.org.in">Mudra</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://venturefund.sidbi.in">SIDBI Venture Fund</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://www.sidbi.in">SIDBI Startup Mitra</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://10000startups.com">NASSCOM – 10000 Startups</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="http://www.investindia.gov.in">Invest India</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="https://www.startupindiahub.org.in">Startup India Hub</a>'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									<a href="www.startup.karnataka.gov.in">Startup Cell Karnataka</a>'+
+'								</p>'+
+'							</div>'+
+''+
+'							<div class="col-xs-12 col-md-4 text-xs-center text-md-left">'+
+'								<p class="mbr-section-subtitle display-3" style="font-size: 21px; margin-top: 30px;">'+
+'									Contact Us'+
+'								</p>'+
+'								<p class="mbr-section-text lead">'+
+'									VidyaSunil & Associates'+
+'									<br>'+
+'									# 64 , Benaka Krupa Apartments'+
+'									<br>'+
+'									7th Main , 7th Cross, Rajeev Nagar'+
+'									<br>'+
+'									Banashankari 3rd Stage'+
+'									<br>'+
+'									Bangalore - 560085'+
+'									<br>'+
+'									E Mail ID : vidyasunilassociatest@gmail.com'+
+'									<br>'+
+'									Cell No. :+91  9739834819 / +91 9480633382'+
+'									<br>'+
+'								</p>'+
+'							</div>'+
+'						</div>'+
+'					</div>'+
+'				</div>'+
+'			<div class="container margin-top-40">'+
+'				<p class="text-xs-center mbr-section-text lead">'+
+'					Copyright (c) 2016 <strong><a href="http://weloftlabs.com" target="_blank">Weloft Labs</a></strong>.'+
+'				</p>'+
+'			</div>'+
+'		</footer>';
+	
+
+
+
+document.getElementById('my-footer').innerHTML+=footer;
+document.getElementById('my-nav').innerHTML+=header;
